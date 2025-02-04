f4ae28abea45c1af92640354e7e1a7809d32ebf9
Pandurang <pandurang@git.training>
2025-02-04 15:09:57 +0530

    oops

---
+++ oops/ems.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/*
+Employee Mangement Syestem 
+Employee : name id salary
+subclasses 
+Manager Engineer Intern
+Override 
+calculateBonus() for each role 
+*/
+
+
+class Employee {
+
+    #
+    salary;
+
+    constructor(name, id) {
+
+    }
+}

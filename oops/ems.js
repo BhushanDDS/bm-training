444335de040c120fe7fee4ccbb23dc39332846cb
Pandurang <pandurang@git.training>
2025-02-04 15:20:41 +0530

    task 1 - OOPS

--- oops/ems.js
+++ oops/ems.js
@@ -1,21 +1,54 @@
-'use strict';
-
-/*
-Employee Mangement Syestem 
-Employee : name id salary
-subclasses 
-Manager Engineer Intern
-Override 
-calculateBonus() for each role 
-*/
-
-
 class Employee {
+    constructor(name, id, salary) {
+        this.name = name;
+        this.id = id;
+        this.#salary = salary;
+    }
 
-    #
-    salary;
+    getSalary() {
+        return this.#salary;
+    }
 
-    constructor(name, id) {
-
+    calculateBonus() {
+        return 0;
     }
 }
+
+class Manager extends Employee {
+    constructor(name, id, salary) {
+        super(name, id, salary);
+    }
+
+    calculateBonus() {
+        return this.getSalary() * 0.2;
+    }
+}
+
+class Engineer extends Employee {
+    constructor(name, id, salary) {
+        super(name, id, salary);
+    }
+
+    calculateBonus() {
+        return this.getSalary() * 0.15;
+    }
+}
+
+class Intern extends Employee {
+    constructor(name, id, salary) {
+        super(name, id, salary);
+    }
+
+    calculateBonus() {
+        return this.getSalary() * 0.5;
+    }
+}
+
+const manager = new Manager("om", 1, 100000);
+console.log(`Manager Bonus: $${manager.calculateBonus()}`);
+
+// const engineer = new Engineer("raj", 2, 80000);
+// console.log(`Engineer Bonus: $${engineer.calculateBonus()}`);
+
+const engineer = new Engineer("raj", 2, 80000);
+console.log(`Engineer Bonus: $${Intern.calculateBonus()}`);

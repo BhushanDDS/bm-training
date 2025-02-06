e4a360e62c665325d6e097f47ab257e014a218bf
Pandurang <pandurang@git.training>
2025-02-06 15:03:07 +0530

    task 1

---
+++ Assighnment_4/task1/script.js
@@ -0,0 +1,28 @@
+const fecthData = new Promise((res, rej) => {
+    try {
+        setTimeout(() => {
+            res(['name1', 'name2', 'name3', 'name4'])
+
+        }, 2000)
+
+    } catch (error) {
+        rej('errorlol')
+    }
+})
+
+fecthData.then(data => {
+    console.log(data);
+}).catch(err => console.log(err))
+
+const consumePromice = async() => {
+    try {
+        let res = await fecthData;
+        console.log(res);
+
+    } catch (err) {
+        console.log(err);
+
+
+    }
+}
+consumePromice();
